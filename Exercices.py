3aab6b10f07116a1583d671e34bc741fef543d15
julienPalleau <jpalleau@gmail.com>
2022-12-02 14:23:40 +0100

    Dictionary methods 2/2

--- Exercices.py
+++ Exercices.py
@@ -3952,6 +3952,40 @@ Finally, print the squares dictionary on the next line after all the changes.
 
 The dictionary squares is already defined for you.
 """
+# # There is no such key
+# squares = {1: 1, 3: 9, 5: 25, 6: 36, 8: 64, 10: 100, 11: 121, 15: 225}
+# value = int(input())
+# return_value = squares.pop(value, 'There is no such key')
+# print(return_value)
+# print(squares)
+
+########################################################################################################################
+"""
+Dictionary methods
+We Are What We Eat
+
+Anthony keeps track of what he eats: he writes down how many calories are in his meals. Use the list of dictionaries to 
+calculate the total amount of calories per day and print it.
+
+The sample input will look like:
+meals = [
+        {"title": "Oatmeal pancakes with apple and cinnamon", "kcal": 370},
+        {"title": "Italian salad with fusilli and ham", "kcal": 320},
+        {"title": "Bulgur with vegetables", "kcal": 350},
+        {"title": "Curd souffle with lingonberries and ginger", "kcal": 225},
+        {"title": "Oatmeal with honey and peanuts", "kcal": 440}]
+"""
+meals = [
+        {"title": "Oatmeal pancakes with apple and cinnamon", "kcal": 370},
+        {"title": "Italian salad with fusilli and ham", "kcal": 320},
+        {"title": "Bulgur with vegetables", "kcal": 350},
+        {"title": "Curd souffle with lingonberries and ginger", "kcal": 225},
+        {"title": "Oatmeal with honey and peanuts", "kcal": 440}]
+
+calories = 0
+for line in meals:
+    calories += line["kcal"]
+print(calories)
 
 ########################################################################################################################
 """
