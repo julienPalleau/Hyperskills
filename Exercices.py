26d7c20762f13b1ae23c82c24c3d0fb7a08d48bc
julienPalleau <jpalleau@gmail.com>
2022-12-09 14:20:37 +0100

    Set operations

--- Exercices.py
+++ Exercices.py
@@ -4334,6 +4334,190 @@ If the volunteer remembers all numbers correctly, print True, otherwise, you sho
 
 ########################################################################################################################
 """
+Set operations
+Match operators
+
+Match the names of the methods with the corresponding operators.
+intersection    &
+union           -
+update          |=
+difference      |
+
+Solution        
+intersection    &
+union           |
+update          |=
+difference      -
+"""
+
+########################################################################################################################
+"""
+Set operations
+Rivers
+
+Some states of the USA share their names with rivers. We have defined two variables with respective place names.
+Print out a new set with river names that don't overlap with given states.
+
+ Sample Input 1:
+Alabama Missouri Mississippi
+Georgia Alaska Missouri
+
+Sample Output 1:
+{'Mississippi', 'Alabama'}
+"""
+
+# # work with these variables
+# rivers = set(input().split())
+# states = set(input().split())
+# print(rivers - states)
+
+########################################################################################################################
+"""
+Set operations
+A bunch of wizards
+
+You are given several sets with names of students in different classes. Output the set containing names of all the 
+students.
+NB! You don't need to read the input; the variables containing the input data are already created for you. 
+
+ Sample Input 1:
+Potter Weasley
+Lovegood Corner
+Malfoy Goyle
+Bones Macmillan
+
+Sample Output 1:
+{'Potter', 'Weasley', 'Lovegood', 'Corner', 'Malfoy', 'Goyle', 'Bones', 'Macmillan'}
+"""
+# gryffindor = set(input().split())
+# ravenclaw = set(input().split())
+# slytherin = set(input().split())
+# hufflepuff = set(input().split())
+# print(gryffindor | ravenclaw | slytherin | hufflepuff)
+
+########################################################################################################################
+"""
+Set operations
+Operation name
+
+What is the name of the operation that returns a new set filled with objects that only one of the original sets had?
+    union
+    intersection_update
+X   difference
+    difference_update
+"""
+
+########################################################################################################################
+"""
+Set operations
+Tourists
+
+Eugene and Rose decided to travel abroad. For convenience, they will go to a country where one of them has already been,
+but not where they both have.
+Output a set of potential countries for them.
+
+ Sample Input 1:
+Greece Netherlands Colombia UK
+Italy UK Russia Greece Canada
+
+Sample Output 1:
+{'Russia', 'Italy', 'Colombia', 'Netherlands', 'Canada'}
+"""
+# # work with these variables
+# eugene = set(input().split())
+# rose = set(input().split())
+# print(rose.symmetric_difference(eugene))
+
+########################################################################################################################
+"""
+Set operations
+Planet sets
+
+Using the three given sets, write a code that creates a set containing the elements that all of the original sets have 
+in common. Output this resulting set.
+
+NB! You don't need to read the input; the variables containing the input data are already created for you. 
+
+ Sample Input 1:
+Jupiter Saturn Mars
+Earth Mars Venus
+Mars Pluto Uranus
+
+Sample Output 1:
+{'Mars'}
+
+"""
+# set_1 = set(input().split())
+# set_2 = set(input().split())
+# set_3 = set(input().split())
+#
+# print(set_1 & set_2 & set_3)
+
+########################################################################################################################
+"""
+Set operations
+A set and a string
+
+Write down the command that should be put instead of ... to get a set of symbols that the set a and the string b both 
+contain.
+
+a = set("my code is brOKen")
+b = "i'm not OK with that"
+c = ...
+
+Do NOT convert b to a set in your solution, it should remain a string.
+"""
+# a = set("my code is brOKen")
+# b = "i'm not OK with that"
+# c = a.intersection(b)
+# print(c)
+
+########################################################################################################################
+"""
+Set operations
+Talents
+
+Imagine you have two lists of names: people from the first list play the violin, while people from the second one happen to speak German.
+Print a set with names of those who can do both.
+
+ Sample Input 1:
+Vanessa-Mae, David Garrett
+Rosamond Mayhan, Malik Kopf, David Garrett, Cortez Mestas, Barbara Miller, Elease Knudson
+
+Sample Output 1:
+{'David Garrett'}
+"""
+# # work with these variables
+# violinists = set(input().split(', '))
+# german_speakers = set(input().split(', '))
+# print(violinists & german_speakers)
+
+########################################################################################################################
+"""
+Set operations
+Hack the Pentagon
+
+There's a hidden variable with top secret data. You can't see it, and all you know about it is that it's called 
+pentagon_passwords and that it is a container with sets. Try to get the intersection of these sets and output the number
+of elements in it. 
+"""
+# pentagon_intersection = set.intersection(*pentagon_passwords)
+# print(len(pentagon_intersection))
+
+########################################################################################################################
+"""
+Set operations
+Operator and method
+
+What is the difference between using an operator and a set operation method?
+X   The difference is in the syntax.
+    Only operators can work with non-set arguments
+    Only operators can be used to update an existing set
+X   Only methods can accept a bunch of sets at once, in a certain way
+"""
+
+########################################################################################################################
+"""
 Work on project. Stage 1/4 Mini-calculator
 Mini-calculator
 
